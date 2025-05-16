ed40847b768b6ae6364f837a4575bca11daf2cac
arnaudvdm123 <arnaud.vandermeersch@student.kuleuven.be>
2025-05-16 16:08:50 +0200

    Update verkiezing Arnaud.py

--- verkiezing.py
+++ verkiezing.py
@@ -6,6 +6,9 @@ class Kandidaat:
     def geef_stem(self, stem):
         self.stemmen.append(stem)
 
+    def aantal_stemmen(self):
+        return len(self.stemmen)
+    
     def __str__(self):
         return f"{self.naam}"
     
@@ -24,4 +27,18 @@ class Kiezer:
     def stem(self, kandidaat):
         stem = Stem(kandidaat)
         kandidaat.geef_stem(stem)
-        print(f"{self.naam} heeft gestemd op {kandidaat}")
+        print(f"{self.naam} heeft gestemd op {kandidaat}"
+
+              
+if __name__ == "__main__":
+    k1 = Kandidaat("Laura")
+    k2 = Kandidaat("Raph")
+    kiezer1 = Kiezer("Thibault")
+    kiezer2 = Kiezer("Alexander")
+
+    kiezer1.stem(k1)
+    kiezer2.stem(k2)
+    kiezer1.stem(k1)
+
+    print(f"{k1} heeft {k1.aantal_stemmen()} stemmen.")
+    print(f"{k2} heeft {k2.aantal_stemmen()} stemmen.")
