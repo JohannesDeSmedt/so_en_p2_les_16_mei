9bcc4b36fdad2e8acb6fc893db9781c6c32ce35f
JesseVerbeeck <jesse.verbeeck@student.kuleuven.be>
2025-05-18 15:30:44 +0200

    update rector

--- rector_verkiezing.py
+++ rector_verkiezing.py
@@ -1,5 +1,5 @@
 from verkiezing import Kandidaat, Stem, Kiezer
-
+import random
 class RectorKandidaat(Kandidaat):
     def __init__(self, naam, faculteit):
         super().__init__(naam)
@@ -15,4 +15,36 @@ class RectorStem(Stem):
 
     def __str__(self):
         return f"Stem op {self.kandidaat} (Rector: {self.faculteit})"
+    
+class RectorKiezer(Kiezer):
+    def __init__(self, naam, faculteit):
+        super().__init__(naam)
+        self.faculteit = faculteit
+
+    def stem(self, kandidaat):
+        stem = RectorStem(kandidaat, self.faculteit)
+        kandidaat.geef_stem(stem)
+        print(f"{self.naam} heeft gestemd op {kandidaat} ({self.faculteit})")    
+   
+kandidatenlijst = [
+    RectorKandidaat("Siemon","Theologie en Religiewetenschappen"),
+    RectorKandidaat("Jonas","Sociale Wetenschappen"),
+    RectorKandidaat("Tom","Letteren"),
+    RectorKandidaat("Femke","Toegepaste Economische Wetenschappen"),
+    RectorKandidaat("Geert","Economie en Bedrijfswetenschappen"),
+    RectorKandidaat("Wilfried","Economie en Bedrijfswetenschappen"),
+]
+lijst_kiezers = [
+    RectorKiezer("Jos","Sociale Wetenschappen"),
+    RectorKiezer("Johan","Letteren"),
+    RectorKiezer("Tommeke","Economie en Bedrijfswetenschappen")
+]
+
+for kiezer in lijst_kiezers:
+    #ik ga het lot laten beslissen op wie de mensen gaan stemmen:
+    #kandidaatnummer die stem krijgt:
+    kandidaatnummer = random.randint(0,len(kandidatenlijst)-1)
+    kiezer.stem(kandidatenlijst[kandidaatnummer])
+    
+     
     
