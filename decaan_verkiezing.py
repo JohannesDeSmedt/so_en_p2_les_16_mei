528000db6495715211b7a99b6d4e462f8e26a398
JesseVerbeeck <jesse.verbeeck@student.kuleuven.be>
2025-05-18 15:02:54 +0200

    Update decaan_verkiezing.py
    commit

--- decaan_verkiezing.py
+++ decaan_verkiezing.py
@@ -1,5 +1,5 @@
 from verkiezing import Kandidaat, Stem, Kiezer
-
+import random
 class DecaanKandidaat(Kandidaat):
     def __init__(self, naam, opleiding):
         super().__init__(naam)
@@ -28,3 +28,25 @@ class DecaanKiezer(Kiezer):
             print(f"{self.naam} heeft gestemd op {kandidaat} ({self.opleiding})")
         else:
             print(f"{self.naam} kan niet stemmen op {kandidaat} ({kandidaat.opleiding})")
+
+kandidatenlijst = [
+    DecaanKandidaat("Siemon","Kinesitherapie"),
+    DecaanKandidaat("Jonas","Informatica"),
+    DecaanKandidaat("Tom","Handelsingenieur"),
+    DecaanKandidaat("Femke","Toegepaste Economische Wetenschappen"),
+    DecaanKandidaat("Geert","Psychologie"),
+    DecaanKandidaat("Wilfried","Marketing"),
+]
+lijst_kiezers = [
+    DecaanKiezer("Jos","Handelsingenieur"),
+    DecaanKiezer("Johan","Marketing"),
+    DecaanKiezer("Tommeke","Psychologie")
+]
+
+for kiezer in lijst_kiezers:
+    #ik ga het lot laten beslissen op wie de mensen gaan stemmen:
+    #kandidaatnummer die stem krijgt:
+    kandiddaatnummer = random.randint(0,len(kandidatenlijst)+1)
+        kiezer.stem(kandidatenlijst[kandidaatnummer])
+    
+    
