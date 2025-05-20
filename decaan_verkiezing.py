3a3af0f43123b00a7c3db27a0437055d8f0f4a2d
Simonlipperts <simon.lipperts@student.kuleuven.be>
2025-05-20 11:03:45 +0200

    taak github
    Inkapseling plus abstractie toegevoegd aan decaan_verkiezing en verkiezing voor github taak

--- decaan_verkiezing.py
+++ decaan_verkiezing.py
@@ -1,30 +1,81 @@
 from verkiezing import Kandidaat, Stem, Kiezer
+import random
+#from faker import Faker
 
 class DecaanKandidaat(Kandidaat):
-    def __init__(self, naam, opleiding):
-        super().__init__(naam)
-        self.opleiding = opleiding
+    def __init__(self, naam_kandidaat, opleiding):
+        super().__init__(naam_kandidaat)
+        self.__opleiding = opleiding
+    
+    @property
+    def get_opleiding(self):
+        return self.__opleiding
 
     def __str__(self):
-        return f"{self.naam} ({self.opleiding})"
+        return super().__str__() + self.get_opleiding
     
 class DecaanStem(Stem):
-    def __init__(self, kandidaat, opleiding):
+    def __init__(self, kandidaat: DecaanKandidaat, opleiding):
         super().__init__(kandidaat)
-        self.opleiding = opleiding
+        self.__opleiding = opleiding
+
+    @property
+    def get_opleiding(self):
+        return self.__opleiding
 
     def __str__(self):
-        return f"Stem op {self.kandidaat} ({self.opleiding})"
+        return super().__str__() + self.get_opleiding
     
 class DecaanKiezer(Kiezer):
     def __init__(self, naam, opleiding):
         super().__init__(naam)
-        self.opleiding = opleiding
+        self.__opleiding = opleiding
+    
+    @property
+    def get_opleiding(self):
+        return self.__opleiding
 
-    def stem(self, kandidaat):
-        if kandidaat.opleiding == self.opleiding:
-            stem = DecaanStem(kandidaat, self.opleiding)
+    def stem(self, kandidaat: DecaanKandidaat):
+        if kandidaat.get_opleiding == self.get_opleiding:
+            stem = DecaanStem(kandidaat, self.get_opleiding) #overschrijft niet, maakt telkens nieuw object aan
             kandidaat.geef_stem(stem)
-            print(f"{self.naam} heeft gestemd op {kandidaat} ({self.opleiding})")
+            print(f"{self.get_naam_kiezer} ({self.get_opleiding}) heeft gestemd op {kandidaat.get_naam_kandidaat} ({self.get_opleiding})")
         else:
-            print(f"{self.naam} kan niet stemmen op {kandidaat} ({kandidaat.opleiding})")
+            print(f"{self.get_naam_kiezer} ({self.get_opleiding}) kan niet stemmen op {kandidaat.get_naam_kandidaat} ({kandidaat.get_opleiding})")
+    
+    def __str__(self):
+        return super().__str__()
+        
+#kandidatenlijst
+kandidatenlijst = [
+    DecaanKandidaat("Laura", "Biotechnologie"),
+    DecaanKandidaat("Michaël", "Artificiële Intelligentie"),
+    DecaanKandidaat("Sanne", "Bedrijfskunde"),
+    DecaanKandidaat("Lars", "Milieuwetenschappen"),
+    DecaanKandidaat("Eline", "Neurowetenschappen"),
+    DecaanKandidaat("Daan", "Handelsingenieur")
+]
+
+#kiezerslijst
+random.seed(42)
+'''vakken = ['Biotechnologie', 'Artificiële Intelligentie', 'Bedrijfskunde', 'Milieuwetenschappen', 'Neurowetenschappen', 'Handelsingenieur']
+fake = Faker('nl_NL')
+kiezerslijst = []
+for i in range(50):
+    kiezerslijst.append(DecaanKiezer(fake.first_name(), random.choice(vakken)))'''
+
+kiezerslijst = [
+    DecaanKiezer("Anna", "Bedrijfskunde"),
+    DecaanKiezer("Victor", "Handelsingenieur"),
+    DecaanKiezer("Nora", "Biotechnologie"),
+    DecaanKiezer("Jan", "Neurowetenschappen")
+]
+
+#Stemprocedure
+stemmen = []
+for kiezer in kiezerslijst:
+    random_kandidaat = random.randint(0, len(kandidatenlijst) - 1)
+    kiezer.stem(kandidatenlijst[random_kandidaat]) #hier zijn beide kiezer en kandidatenlijst[random_kandidaat] objecten
+
+for kandidaat in kandidatenlijst:
+    print(f'{kandidaat} heeft zoveel stemmen: {len(kandidaat.get_stemmen)}')
