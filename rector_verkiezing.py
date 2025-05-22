942e5cb4637a30a4b951cbd7387c49f82e018697
jorenVerlackt <joren.verlackt@student.kuleuven.be>
2025-05-22 23:20:33 +0200

    rector

--- rector_verkiezing.py
+++ rector_verkiezing.py
@@ -3,16 +3,58 @@ from verkiezing import Kandidaat, Stem, Kiezer
 class RectorKandidaat(Kandidaat):
     def __init__(self, naam, faculteit):
         super().__init__(naam)
-        self.faculteit = faculteit
+        self._faculteit = faculteit
+
+    def get_faculteit(self):
+        return self._faculteit
 
     def __str__(self):
-        return f"{self.naam} (Rector: {self.faculteit})"
-    
+        return f"{self.get_naam()} (Rector: {self._faculteit})"
+
 class RectorStem(Stem):
     def __init__(self, kandidaat, faculteit):
         super().__init__(kandidaat)
-        self.faculteit = faculteit
+        self._faculteit = faculteit
+
+    def get_faculteit(self):
+        return self._faculteit
 
     def __str__(self):
-        return f"Stem op {self.kandidaat} (Rector: {self.faculteit})"
-    
+        return f"Stem op {self.get_kandidaat()} (Rector: {self._faculteit})"
+
+class RectorKiezer(Kiezer):
+    def __init__(self, naam, faculteit):
+        super().__init__(naam)
+        self._faculteit = faculteit
+
+    def get_faculteit(self):
+        return self._faculteit
+
+    def stem(self, kandidaat):
+        if kandidaat.get_faculteit() == self._faculteit:
+            stem = RectorStem(kandidaat, self._faculteit)
+            kandidaat.geef_stem(stem)
+            print(f"{self.get_naam()} heeft gestemd op {kandidaat} ({self._faculteit})")
+        else:
+            print(f"{self.get_naam()} kan niet stemmen op {kandidaat} ({kandidaat.get_faculteit()})")
+
+class RectorVerkiezing:
+    def __init__(self):
+        self._kandidaten = []
+        self._kiezers = []
+
+    def voeg_kandidaat_toe(self, kandidaat):
+        self._kandidaten.append(kandidaat)
+
+    def voeg_kiezer_toe(self, kiezer):
+        self._kiezers.append(kiezer)
+
+    def laat_stemmen(self):
+        for kiezer in self._kiezers:
+            for kandidaat in self._kandidaten:
+                if self.mag_stemmen(kiezer, kandidaat):
+                    kiezer.stem(kandidaat)
+                    break
+
+    def mag_stemmen(self, kiezer, kandidaat):
+        return kiezer.get_faculteit() == kandidaat.get_faculteit()
--- verkiezing.py
+++ verkiezing.py
@@ -1,27 +1,38 @@
 class Kandidaat:
     def __init__(self, naam):
-        self.naam = naam
-        self.stemmen = []
+        self._naam = naam
+        self._stemmen = []
+
+    def get_naam(self):
+        return self._naam
 
     def geef_stem(self, stem):
-        self.stemmen.append(stem)
+        self._stemmen.append(stem)
+
+    def get_stemmen(self):
+        return self._stemmen
 
     def __str__(self):
-        return f"{self.naam}"
+        return f"{self._naam}"
     
 class Stem:
     def __init__(self, kandidaat):
-        self.kandidaat = kandidaat
+        self._kandidaat = kandidaat
+
+    def get_kandidaat(self):
+        return self._kandidaat
 
     def __str__(self):
-        return f"Stem op {self.kandidaat}"
+        return f"Stem op {self._kandidaat}"
     
 class Kiezer:
     def __init__(self, naam):
-        self.naam = naam
+        self._naam = naam
 
+    def get_naam(self):
+        return self._naam
 
     def stem(self, kandidaat):
         stem = Stem(kandidaat)
         kandidaat.geef_stem(stem)
-        print(f"{self.naam} heeft gestemd op {kandidaat}")
+        print(f"{self.get_naam()} heeft gestemd op {kandidaat}")
